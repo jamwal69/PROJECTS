fb0ad77660ed154fa5c6fbe26699b17a306464d2
MANAV JAMWAL <95372867+jamwal69@users.noreply.github.com>
2022-09-08 23:38:22 +0530

    Update HANGMAN.C

--- HANGMAN.C
+++ HANGMAN.C
@@ -1,201 +1,117 @@
 #include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
 #include<string.h>
-typedef struct{
-	char name[50];
-	int bus_num;
-	int num_of_seats;
-}pd;
-void reservation(void);
-void viewdetails(void);
-void printticket(char name[],int,int,float);
-void specificbus(int);
-float charge(int,int);
-int main()
 
+void showHangman(int);
+
+int main(void)
 {
-		system("cls");
-	printf("\t\t|           BUS TICKET RERSERVATION SYSTEM           |\n");
-    printf("\t\t|                            By CSE TEAM           |\n");
-    printf("\t\t|                            MIET , JAMMU         |\n");
-    printf("\t\t------------------------------------------------------\n");
-    printf("\t\t---------------press any key to continue--------------\n");
-    printf("\t\t------------------------------------------------------\n");
-	getch();
-    system("cls");
-	int menu_choice,choice_return;
-    start:
-	system("cls");
-	printf("\n=================================\n");
-	printf("    BUS RESERVATION SYSTEM");
-	printf("\n=================================");
-	printf("\n1>> Reserve A Ticket");
-	printf("\n------------------------");
-	printf("\n2>> View All Available Bus");
-	printf("\n------------------------");
-	printf("\n3>> Exit");
-	printf("\n------------------------");
-	printf("\n\n-->");
-	scanf("%d",&menu_choice);
-	switch(menu_choice)
-	{
-		case 1:
-			reservation();
-			break;
-		case 2:
-			viewdetails();
-			printf("\n\nPress any key to go to Main Menu..");
-			getch();
-			break;
-		case 3:
-			return(0);
-		default:
-			printf("\nInvalid choice");
-	}
-	goto start;
-	return(0);
-}
+    char tempWord[100], hangmanOutput[100], alphabetFromUser, hangmanWord[100]="hello";
+    int wrongTry = 6 , matchFound = 0, counter = 0 , position = 0, winner, length , i;
 
-void viewdetails(void)
-{
-	system("cls");
-	printf("\nBUS.No\tBUS Name\tDestinations\t\t\tfare\t\t\tTime\n");
-	printf("\n101\tBus A\tTRIKUTA NAGAR to GANDHI NAGAR\t\t Rs.5000\t\t9am");
-	printf("\n102\tBus B\tGANDHI NAGAR to NANAK NAGAR\t\t Rs.5000\t\t12pm");
-	printf("\n103\tBus C\tCHANNI HIMMAT to KOT BALWAL\t\t Rs.4500\t\t8am");
-	printf("\n104\tBus D\tTRIKUTA NAGAR to KOT BALWAL\t\t Rs.4500\t\t11am");
-	printf("\n105\tBus E\tNANAK NAGAR to KOT BALWAL\t\t Rs.5000\t\t7am");
-}
-void reservation(void)
-{
-	char confirm;
-	int i=0;
-	float charges;
-	pd passdetails;
-	FILE *fp;
-	fp=fopen("seats_reserved.txt","a");
-	system("cls");
+    printf("\n\n\tHIT >>ENTER<<");
 
-	printf("\nEnter Your Name:> ");
-	fflush(stdin);
-	gets(passdetails.name);
+    getchar();       
 
-	printf("\nEnter Number of seats:> ");
-	scanf("%d",&passdetails.num_of_seats);
-	printf("\n\n>>Press Enter To View Available Bus<< ");
-	getch();
-	system("cls");
-	viewdetails();
-	printf("\n\nEnter bus number:> ");
-	start1:
-	scanf("%d",&passdetails.bus_num);
-	if(passdetails.bus_num>=101 && passdetails.bus_num<=105)
-	{
-		charges=charge(passdetails.bus_num,passdetails.num_of_seats);
-		printticket(passdetails.name,passdetails.num_of_seats,passdetails.bus_num,charges);
-	}
-	else
-	{
-		printf("\nInvalid bus Number! Enter again--> ");
-		goto start1;
-	}
+    length = strlen(hangmanWord);               
 
-	printf("\n\nConfirm Ticket (y/n):>");
-	start:
-	scanf(" %c",&confirm);
-	if(confirm == 'y')
-	{
-		fprintf(fp,"%s\t\t%d\t\t%d\t\t%.2f\n",&passdetails.name,passdetails.num_of_seats,passdetails.bus_num,charges);
-		printf("==================");
-		printf("\n Reservation successful\n");
-		printf("==================");
-		printf("\nPress any key to go back to Main menu");
-	}
-	else
-	{
-		if(confirm=='n'){
-			printf("\nReservation Not Done!\nPress any key to go back to  Main menu!");
-		}
-		else
-		{
-			printf("\nInvalid choice entered! Enter again-----> ");
-			goto start;
-		}
-	}
-	fclose(fp);
-	getch();
-}
+    printf("\n\n !!!!!!!!!!!!!!!!!!!Welcome to the HANGMAN GAME!!!!!!!!!!!!!!!!!\n\n\n");
+    printf("\n\n You will get 5 chances to guess the right word");
 
-float charge(int bus_num,int num_of_seats)
-{
-		if (bus_num==101)
-	{
-		return(5000.0*num_of_seats);
-	}
-	if (bus_num==102)
-	{
-		return(5000.0*num_of_seats);
-	}
-	if (bus_num==103)
-	{
-		return(4500.0*num_of_seats);
-	}
-	if (bus_num==104)
-	{
-		return(4500.0*num_of_seats);
-	}
-	if (bus_num==105)
-	{
-		return(5000.0*num_of_seats);
-	}
-}
+    getchar();
 
-void printticket(char name[],int num_of_seats,int bus_num,float charges)
-{
-	system("cls");
-	printf("-------------------\n");
-	printf("\tTICKET\n");
-	printf("-------------------\n\n");
-	printf("Name:\t\t\t%s",name);
-	printf("\nNumber Of Seats:\t%d",num_of_seats);
-	printf("\nBus Number:\t\t%d",bus_num);
-	specificbus(bus_num);
-	printf("\nCharges:\t\t%.2f",charges);
-}
+    printf("\n\n\tHIT >>ENTER<< ");
 
-void specificbus(int bus_num)
-{
+    getchar();
 
-	if (bus_num==101)
-	{
-		printf("\nBus:\t\t\tBUS A");
-		printf("\nDestination:\t\tTRIKUTA NAGAR to GANDHI NAGAR");
-		printf("\nDeparture:\t\t9am ");
-	}
-	if (bus_num==102)
-	{
-		printf("\nBus:\t\t\tBUS B");
-		printf("\nDestination:\t\tGANDHI NAGAR to NANAK NAGAR");
-		printf("\nDeparture:\t\t12pm");
-	}
-	if (bus_num==103)
-	{
-		printf("\nBus:\t\t\tBUS C");
-		printf("\nDestination:\t\tCHANNI HIMMAT to KOT BALWAL");
-		printf("\nDeparture:\t\t8am");
-	}
-	if (bus_num==533104)
-	{
-		printf("\nBus:\t\t\tBUS D");
-		printf("\nDestination:\t\tTRIKUTA NAGAR to KOT BALWAL");
-		printf("\nDeparture:\t\t11am ");
-	}
-	if (bus_num==105)
-	{
-		printf("\nBus:\t\t\tBUS C");
-		printf("\nDestination:\t\tNANAK NAGAR to KOT BALWAL");
-		printf("\nDeparture:\t\t7am");
-	}
-}
+    printf("\n\t||===== ");                 
+	printf("\n\t\t||    | ");
+    printf("\n\t||      ");
+    printf("\n\t||      ");
+    printf("\n\t||      ");
+    printf("\n\t||      ");
 
+    printf("\n\n     The word has %d alphabets \n\n",length);  
+
+    for( i = 0; i < length ; i++)
+    {
+        hangmanOutput[i] = '_';
+        hangmanOutput[length] = '\0';
+    }
+
+    for(i = 0 ; i < length ; i++)
+    {
+        printf(" ");
+        printf("%c",hangmanOutput[i]);
+    }
+    while(wrongTry != 0)
+    {
+        matchFound = 0;
+        printf("\n\n   enter any alphabet from a to z and please use small case!!");
+        printf("\n\n\t Enter HERE ==> ");
+	    scanf("%c",&alphabetFromUser);
+
+        if(alphabetFromUser < 'a' || alphabetFromUser > 'z')
+        {
+            printf("\n\n\t invalid input, please enter an aplhabet ");
+            matchFound = 2;
+        }
+        if (matchFound != 2)
+        {
+            for(counter=0;counter<length;counter++)    
+	        {
+		        if(alphabetFromUser==hangmanWord[counter])
+		        {
+		            matchFound = 1;
+                }
+            }
+
+	    if(matchFound == 0) 
+	    {
+     	    printf("\n\t :( You have %d tries left ",--wrongTry);
+	        getchar();
+            showHangman(wrongTry);
+            getchar();
+	    }
+
+	   else
+	   {
+	        for(counter = 0; counter < length; counter++)
+            {
+     	        matchFound = 0;
+                if(alphabetFromUser == hangmanWord[counter])
+	            {
+     		        position = counter ;
+     		        matchFound = 1;
+	            }
+    	        if(matchFound == 1)
+	            {
+                    for(i = 0 ; i < length ; i++)
+                    {
+                        if( i == position)
+                  	    {
+                            hangmanOutput[i] = alphabetFromUser; 
+                        }
+                        else if( hangmanOutput[i] >= 'a' && hangmanOutput[i] <= 'z' ) 
+                        {
+                            continue;
+                  	    }
+                        else
+                        {
+                            hangmanOutput[i] = '_';
+                        }
+                    }
+                    tempWord[position] = alphabetFromUser;
+                    tempWord[length] = '\0'; 
+                    winner = strcmp(tempWord,hangmanWord);
+
+                    if(winner == 0)
+                    {
+                        printf("\n\n\t \t YAHOO!!!!! You are the WINNER !!!!!");
+                        printf("\n\n\t The Word was %s ",hangmanWord);
+                        getchar();
+                        return 0;
+                    }
+	            }
+	        }
+        }
+    }
